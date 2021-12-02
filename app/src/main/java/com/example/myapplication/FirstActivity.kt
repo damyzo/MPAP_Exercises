40204d324b281d5e4702f8294b7fc8645c681644
damyzo <ilievskidamjan4@gmail.com>
2021-12-02 19:29:01 +0100

    First Exercise Finished

---
+++ app/src/main/java/com/example/myapplication/FirstActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+
+        val usernameControl = findViewById<TextView>(R.id.username)
+        val bundle: Bundle? = intent.extras
+        if(bundle != null){
+            val username = bundle.get("UserName")
+            usernameControl.text = String.format("%s %s", "Hello", username.toString())
+        }
+
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,23 +8,72 @@ import android.os.PersistableBundle
 import android.provider.ContactsContract
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn = findViewById<Button>(R.id.button)
+        val firstButton: Button = findViewById(R.id.firstButton)
+        val nameControl: TextView = findViewById(R.id.name)
+        val implicitButton: Button = findViewById(R.id.implicitButton)
 
-        btn.setOnClickListener(){
-            val intent = Intent(Intent.ACTION_VIEW, ContactsContract.Contacts.CONTENT_URI)
+        firstButton.setOnClickListener(){
+            val firstIntent = Intent(this, FirstActivity::class.java)
+            val nameValue = nameControl.text
+            firstIntent.putExtra("UserName",nameValue)
+            startActivity(firstIntent)
+        }
 
+        implicitButton.setOnClickListener(){
+            val secondIntent = Intent(Intent.ACTION_VIEW)
+
+            secondIntent.data = Uri.parse("https://www.javatpoint.com/")
+
+            startActivity(secondIntent)
 
         }
 
 
-
     }
+
+
+
+//    override fun onStart() {
+//        super.onStart()
+//        Log.println(Log.VERBOSE,"V", "On Start")
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//        Log.println(Log.VERBOSE,"V", "On Resume")
+//
+//    }
+//
+//    override fun onPause() {
+//        super.onPause()
+//        Log.println(Log.VERBOSE,"V", "On Pause")
+//
+//    }
+//
+//    override fun onStop() {
+//        super.onStop()
+//        Log.println(Log.VERBOSE,"V", "On Stop")
+//
+//    }
+//
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        Log.println(Log.VERBOSE,"V", "On Destroy")
+//
+//    }
+//
+//    override fun onRestart() {
+//        super.onRestart()
+//        Log.println(Log.VERBOSE,"V", "On Restart")
+//
+//    }
 
 
 
